0c70adf073fe480963d1a1ee0da403c46461b091
junai <junai@Junaid>
2025-09-25 08:18:41 +0530

    code commited

---
+++ M36/src/test/java/GitHubPushTest/GitHubDummyPush.java
@@ -0,0 +1,13 @@
+package GitHubPushTest;
+
+public class GitHubDummyPush {
+
+	public static void main(String[] args) {
+		System.out.println("DummuyTest 1");
+		System.out.println("DummuyTest 2");
+		System.out.println("DummuyTest 3");
+		System.out.println("DummuyTest 4");
+
+	}
+
+}
